a2ba4b6b11df62a4c088e9d24fa642e115d6c3d1
ChayaHenner <chayahenner@gmail.com>
2023-11-29 22:30:34 +0200

    toys simple work

--- models/toyModel.js
+++ models/toyModel.js
@@ -19,7 +19,6 @@ exports.validateToy = (_reqBody) => {
         name: Joi.string().min(2).max(99).required(),
         info: Joi.string().min(3).max(100).required(),
         category: Joi.string().min(3).max(15).required(),
-        img_url: Joi.string().allow(null, "").max(500),
         price: Joi.number().min(1).max(9999).required()
     })
     return schemaJoi.validate(_reqBody);
--- routes/toys.js
+++ routes/toys.js
@@ -1,8 +1,46 @@
 const express= require("express");
 const router = express.Router();
+const {auth, authAdmin} = require("../middlewares/auth");
+const {ToyModel,validateToy} = require("../models/toyModel")
+const bcrypt = require("bcrypt");
+
+
 
 router.get("/" , (req,res)=> {
   res.json({msg:"Rest api work !"})
+})
+router.get("/search" , async(req,res)=> {
+  let perPage = req.query.perPage || 10;
+  let page = req.query.page || 1;
+    try{
+      let queryS = req.query.s;
+      let searchReg = new RegExp(queryS,"i")
+      let data = await ToyModel.find({$or:[{name:searchReg},{info:searchReg}]})
+      .limit(perPage)
+      .skip((page - 1) * perPage)
+      .sort({_id:-1})
+      res.json(data);
+    }
+    catch(err){
+      console.log(err);
+      res.status(500).json({msg:"server error .cant complete your search",err})
+    }
+})
+
+router.post("/", async(req,res) => {
+  let validBody = validateToy(req.body);
+  if(validBody.error){
+    return res.status(400).json(validBody.error.details);
+  }
+  try{
+    let user = new ToyModel(req.body);
+    await user.save();
+    res.status(201).json(user);
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({msg:"server error. was valid but did not create toy",err})
+  }
 })
 
 module.exports = router;
