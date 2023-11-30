2126771f722ba811be34860d014dfa0c5e782497
ChayaHenner <chayahenner@gmail.com>
2023-11-30 11:41:02 +0200

    HIDE

--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -7,7 +7,6 @@ async function main() {
     mongoose.set('strictQuery', true);
   // await mongoose.connect('mongodb://127.0.0.1:27017/project');
     await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@clusterchaya.zoha4yp.mongodb.net/class_29`);
-    // await mongoose.connect(`mongodb+srv://chayahenner:Russia2023@clusterchaya.zoha4yp.mongodb.net/class_29`);
 
 
   console.log("mongo connect started");
